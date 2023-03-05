ffcce66904ec9dc1c9e53fdbbfcdd23b136ff516
Thinh <73821256+tranthinh-coding@users.noreply.github.com>
2023-03-05 16:21:55 +0700

    feat(request): register Request

--- app/Http/Requests/ApiRegisterRequest.php
+++ app/Http/Requests/ApiRegisterRequest.php
@@ -6,27 +6,28 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ApiRegisterRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
+  /**
+   * Determine if the user is authorized to make this request.
+   *
+   * @return bool
+   */
+  public function authorize()
+  {
+    return true;
+  }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, mixed>
-     */
-    public function rules()
-    {
-        return [
-            'name' => 'required',
-            'email' => 'required|email|unique:users',
-            'password' => 'required',
-        ];
-    }
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array<string, mixed>
+   */
+  public function rules()
+  {
+    return [
+      'name' => 'required|string|max:255',
+      'email' => 'required|string|email|max:255|unique:users',
+      'password' => 'required|string|min:8|confirmed',
+      'password_confirmation' => 'required',
+    ];
+  }
 }
